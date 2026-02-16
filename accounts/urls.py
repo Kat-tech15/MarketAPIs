90d6a1dccf33a38e43f9ee3b7e92262a4bb3a608
Kat-tech15 <kelvinkatwai@gmail.com>
2026-02-16 11:38:27 +0300

    cresated cart app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from .views import RegisterView,LoginView, LogoutView
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('logout/', LogoutView.as_view()),
-]
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('api/token/obtain/', TokenObtainPairView().as_view, name='token-obtain-pair'),
+    path('api/token/refresh/', TokenRefreshView().as_view, name='token-refresh'),
+    path('api/token/verify/', TokenVerifyView().as_view, name='token-verify'),
+] 
